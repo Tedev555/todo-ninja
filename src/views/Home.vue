786b2b2120183d97c53374b9e6fc3c4aa481d67b
ted555 <thanong984@gmail.com>
2019-04-23 09:40:23 +0700

    Working with text and color

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,19 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <h1>HomePage</h1>
+    <p class="red white--text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem cum rem, molestias dolores perferendis nobis labore quos omnis vitae, hic, consequuntur qui saepe doloremque similique velit nulla officia dolorum ullam.</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolorum iure eaque ratione vel, assumenda, culpa blanditiis odio maxime officia quos doloribus distinctio provident tempore excepturi aliquam veniam tempora minus?</p>
+    <h1 class="display-4">Massive Display</h1> 
+    <h4 class="display-1">Smaller Display</h4> 
+    <p class="headline">This is a headline</p>
+    <p class="subheading">This is a subheading</p>
+    <p class="caption">This is a caption</p>
+    <p class="font-wieght-blod">This is a blod text</p>
+
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
   export default {
-    components: {
-      HelloWorld
-    }
   }
 </script>
