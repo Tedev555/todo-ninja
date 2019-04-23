1250ce5aaadd8c19142f710198f6f76546e0211e
ted555 <thanong984@gmail.com>
2019-04-23 13:52:11 +0700

    routing and margin

---
+++ src/views/Team.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="projects">
+    <h1 class="subheading grey--text">Team</h1>
+    <v-container class="my-5">
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi aspernatur labore magnam voluptate placeat. Odit obcaecati quas velit. Perferendis modi eaque quasi porro vitae blanditiis accusamus, omnis enim impedit quaerat.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi aspernatur labore magnam voluptate placeat. Odit obcaecati quas velit. Perferendis modi eaque quasi porro vitae blanditiis accusamus, omnis enim impedit quaerat.</p>
+    </v-container>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
