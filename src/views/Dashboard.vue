c61f03be04e924a54f3307e4f2b17a516e968b5d
ted555 <thanong984@gmail.com>
2019-04-23 14:49:06 +0700

    Grid System 2

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,29 +4,26 @@
 
     <v-container class="my-5">
 
-      <v-layout row wrap>
-        <v-flex xs12 md6>
-          <v-btn outline block class="primary">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class="primary">2</v-btn>
-        </v-flex>
-        <v-flex xs4md2>
-          <v-btn outline block class="primary">2</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class="primary">2</v-btn>
-        </v-flex>
-      </v-layout>
-
-      <v-layout row wrap justify-space-around>
-        <v-flex xs4 md3>
-          <v-btn outline block class="success">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md3>
-          <v-btn outline block class="success">2</v-btn>
-        </v-flex>
-      </v-layout>
+      <v-card flat class="pa-3">
+        <v-layout row wrap>
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project title</div>
+            <div>Create a website</div>
+          </v-flex>
+          <v-flex xs6 md2 sm4>
+            <div class="caption grey--text">Person</div>
+            <div>The net ninja</div>
+          </v-flex>
+          <v-flex xs6 md2 sm4>
+            <div class="caption grey--text">Due by</div>
+            <div>1st Jan 2019</div>
+          </v-flex>
+          <v-flex xs2 md2 sm4>
+            <div class="caption grey--text">Status</div>
+            <div>Ongoing</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
 
     </v-container>
   </div>
