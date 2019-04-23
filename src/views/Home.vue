6d71324556e0cd0bd71d3787ab104127d39f8e27
ted555 <thanong984@gmail.com>
2019-04-23 11:02:06 +0700

    Lists

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="home">
     <h1>HomePage</h1>
-
-    <v-btn class="pink white--text">Click me</v-btn>
-    <v-btn depressed color="pink">Click me</v-btn>
-    <v-btn flat color="pink">Click me</v-btn>
-
-    <v-btn depressed color="pink white--text">
-      <v-icon left>email</v-icon>
-      <span>email me</span>
-    </v-btn>
-
-    <v-btn depressed small color="pink white--text">
-      <v-icon left small>email</v-icon>
-      <span>email me</span>
-    </v-btn>
-
-    <v-btn depressed large color="pink white--text">
-      <span>email me</span>
-      <v-icon right large>email</v-icon>
-    </v-btn>
-
-    <v-btn fab depressed small dark color="purple">
-      <v-icon>favorite</v-icon>
-    </v-btn>
-
   </div>
 </template>
 
